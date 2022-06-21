8862a1850a66d65f675bd386ec9ead7ddd38c458
Ahmed Almory <ahmedjoda02@gmail.com>
2022-06-21 21:26:31 +0200

    fix a misspelling

--- src/Scaffold/Tables/Table.php
+++ src/Scaffold/Tables/Table.php
@@ -3,63 +3,74 @@
 
 namespace Syscape\Single\Scaffold\Tables;
 
-
 use Syscape\Single\Scaffold\Fields\Field;
 
 class Table
 {
-    protected $fields ,
-        $model ,
-        $route ,
-        $table_view = 'single::tables.table',
-        $pagiantion_length = 10 ,
-        $table_data = null;
+    protected $fields ;
+    protected $model ;
+    protected $route ;
+    protected $table_view = 'single::tables.table';
+    protected $pagination_length = 10 ;
+    protected $table_data = null;
     public function __construct($fields)
     {
         $this->fields = $fields;
     }
 
-    public static function make($fields){
-        $fields = array_filter($fields,function (Field $field){
+    public static function make($fields)
+    {
+        $fields = array_filter($fields, function (Field $field) {
             return $field->showingInIndex();
         });
         return new self($fields);
     }
-    public function render(){
-        return view($this->table_view,['table'=>$this]);
+    public function render()
+    {
+        return view($this->table_view, ['table'=>$this]);
     }
-    public function model($model){
+    public function model($model)
+    {
         $this->model = $model;
         return $this;
     }
-    public function route($route){
+    public function route($route)
+    {
         $this->route = $route;
         return $this;
     }
-    public function getQuery(){
+    public function getQuery()
+    {
         return $this->model::query();
     }
-    public function data($data){
+    public function data($data)
+    {
         $this->table_data = $data;
         return $this;
     }
-    public function getData(){
+    public function getData()
+    {
         return $this->table_data ?? $this->getModel()::all();
     }
-    public function getFields(){
+    public function getFields()
+    {
         return $this->fields;
     }
-    public function getRoute(){
+    public function getRoute()
+    {
         return $this->route;
     }
-    public function getModel(){
+    public function getModel()
+    {
         return $this->model;
     }
-    public function paginate($count){
-        $this->pagiantion_length = $count;
+    public function paginate($count)
+    {
+        $this->pagination_length = $count;
         return $this;
     }
-    public function getPaginationLength(){
-        return $this->pagiantion_length;
+    public function getPaginationLength()
+    {
+        return $this->pagination_length;
     }
 }
